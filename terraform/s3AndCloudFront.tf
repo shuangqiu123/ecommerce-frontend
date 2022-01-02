efa9664aa6e72d47c1eced4dfa14230db8510859
shuangqiu123 <qiushuang0625@gmail.com>
2022-01-02 14:02:52 +1100

    add cloudfront error redirect

--- terraform/s3AndCloudFront.tf
+++ terraform/s3AndCloudFront.tf
@@ -87,6 +87,12 @@ resource "aws_cloudfront_distribution" "demostore_distribution" {
     }
   }
   
+  custom_error_response {
+    error_code = 403
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+
   viewer_certificate {
     acm_certificate_arn      = var.FRONTEND_CERT_ARN
     ssl_support_method       = "sni-only"
